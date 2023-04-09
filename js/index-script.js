f908b054e2722af0d1a7239c86b4758ba7a2c8b6
Laahini <62526180+Laahini@users.noreply.github.com>
2023-04-09 14:16:50 -0400

    odyssey 1

---
+++ js/index-script.js
@@ -0,0 +1,35 @@
+const animatedElements = document.querySelectorAll('.animated-element');
+
+window.addEventListener('scroll', () => {
+  animatedElements.forEach(element => {
+    const elementTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (elementTop < windowHeight) {
+      element.classList.add('animate__animated', 'animate__bounceIn');
+    }
+  });
+});
+
+
+
+
+//countdown
+var countDownDate = new Date("May 11, 2023 15:37:25").getTime();
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
